90a4f3117c2d8bf799dada07110b26a29f9e6bb1
Yura Avramenko <jaerbi42@gmail.com>
2019-07-18 21:58:20 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'jaerbi-world';
+export class AppComponent implements OnInit{
+
+  title: string = 'Hello Jaerbi World';
+  name = 'jaerbi';
+
+  ngOnInit(): void {
+  }
+
 }
